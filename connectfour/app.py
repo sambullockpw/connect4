a1a464a8062879f71a23fcd732b06b805328a179
Sam Bullock <sam.b.bullock@gmail.com>
2022-02-27 19:06:48 +1300

    Deploy to heroku

--- connectfour/app.py
+++ connectfour/app.py
@@ -2,6 +2,8 @@
 import itertools
 import json
 import asyncio
+import os
+import signal
 from ntpath import join
 from sqlite3 import connect
 import websockets
@@ -196,8 +198,14 @@ async def handler1(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
