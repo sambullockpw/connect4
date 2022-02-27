0df35c405c8d7684e8b0b8c8936eb0aa9484faa7
Sam Bullock <sam.b.bullock@gmail.com>
2022-02-27 22:18:32 +1300

    set up webapp for deployment

--- connectfour/main.js
+++ connectfour/main.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const board = document.querySelector(".board");
     createBoard(board);
     // Open the WebSocket connection and register event handlers.
-    const websocket = new WebSocket("ws://localhost:8001/");
+    const websocket = new WebSocket(getWebSocketServer());
     initGame(websocket)
     receiveMoves(board, websocket)
     sendMoves(board, websocket);
@@ -74,4 +74,14 @@ function sendMoves(board, websocket) {
       }
       websocket.send(JSON.stringify(event));
     });
+  }
+
+  function getWebSocketServer() {
+    if (window.location.host === "sambullockpw.github.io") {
+      return "wss://connect1111.herokuapp.com/";
+    } else if (window.location.host === "localhost:8000") {
+      return "ws://localhost:8001/";
+    } else {
+      throw new Error(`Unsupported host: ${window.location.host}`);
+    }
   }
